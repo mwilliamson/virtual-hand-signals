feaa3bbb298f920304d25674222aff1d0d9df396
Michael Williamson <mike@zwobble.org>
2021-02-12 18:57:52 +0000

    Create function for creating server

--- server/server.ts
+++ server/server.ts
@@ -5,51 +5,58 @@ import serveHandler from "serve-handler";
 import WebSocket from "ws";
 
 const webSocketPath = "/websocket";
-const port = process.env.PORT || 8000;
 
-const server = http.createServer(function (request, response) {
-    serveHandler(request, response, {
-        cleanUrls: false,
-    });
-});
+function createServer({port}: {port: number}) {
 
-console.log(`Server URI: ws://0.0.0.0:${port}${webSocketPath}`);
-
-const wss = new WebSocket.Server({noServer: true});
-
-const messages: Array<string> = [];
-
-wss.on("connection", function connection(ws) {
-    const intervalId = setInterval(() => {
-        ws.ping();
-    }, 1000);
-
-    messages.forEach(message => ws.send(message));
-
-    ws.on("close", () => {
-        clearInterval(intervalId);
-    });
-
-    ws.on("message", function incoming(payload) {
-        const message = JSON.stringify({
-            index: messages.length,
-            payload: payload,
+    const server = http.createServer(function (request, response) {
+        serveHandler(request, response, {
+            cleanUrls: false,
         });
-        messages.push(message);
-        wss.clients.forEach((ws) => ws.send(message));
     });
-});
 
-server.on("upgrade", function upgrade(request, socket, head) {
-    const requestPath = url.parse(request.url).pathname;
+    console.log(`Server URI: ws://0.0.0.0:${port}${webSocketPath}`);
 
-    if (requestPath === webSocketPath) {
-        wss.handleUpgrade(request, socket, head, function done(ws) {
-            wss.emit("connection", ws, request);
+    const wss = new WebSocket.Server({noServer: true});
+
+    const messages: Array<string> = [];
+
+    wss.on("connection", function connection(ws) {
+        const intervalId = setInterval(() => {
+            ws.ping();
+        }, 1000);
+
+        messages.forEach(message => ws.send(message));
+
+        ws.on("close", () => {
+            clearInterval(intervalId);
         });
-    } else {
-        socket.destroy();
-    }
-});
 
-server.listen(port);
+        ws.on("message", function incoming(payload) {
+            const message = JSON.stringify({
+                index: messages.length,
+                payload: payload,
+            });
+            messages.push(message);
+            wss.clients.forEach((ws) => ws.send(message));
+        });
+    });
+
+    server.on("upgrade", function upgrade(request, socket, head) {
+        const requestPath = url.parse(request.url).pathname;
+
+        if (requestPath === webSocketPath) {
+            wss.handleUpgrade(request, socket, head, function done(ws) {
+                wss.emit("connection", ws, request);
+            });
+        } else {
+            socket.destroy();
+        }
+    });
+
+    server.listen(port);
+}
+
+if (require.main === module) {
+    const port = parseInt(process.env.PORT || "8000", 10);
+    createServer({port: port});
+}
