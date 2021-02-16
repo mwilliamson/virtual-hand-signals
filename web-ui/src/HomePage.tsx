102d01b5b13f89f342e721bc697ea30b051f742f
Michael Williamson <mike@zwobble.org>
2021-02-16 17:47:35 +0000

    Show error when failing to start meeting

--- web-ui/src/HomePage.tsx
+++ web-ui/src/HomePage.tsx
@@ -1,13 +1,31 @@
-import { Button, ButtonGroup, Center } from "@chakra-ui/react";
+import { Button, ButtonGroup, Center, useToast } from "@chakra-ui/react";
 import { useHistory } from "react-router-dom";
+import { useState } from "react";
 
+import { Meeting } from "../../server/lib/meetings";
 import * as api from "./api";
 
 export default function HomePage() {
     const history = useHistory();
 
+    const [error, setError] = useState<Error | null>(null);
+    const toast = useToast();
+
     async function handleStartMeeting() {
-        const meeting = await api.startMeeting();
+        let meeting: Meeting;
+        try {
+            meeting = await api.startMeeting();
+        } catch (error) {
+            console.error(error);
+            setError(error);
+            toast({
+                title: "Could not start meeting",
+                status: "error",
+                isClosable: true,
+                duration: null,
+            });
+            return;
+        }
         history.push(`/meetings/${meeting.meetingCode}`);
     }
 
