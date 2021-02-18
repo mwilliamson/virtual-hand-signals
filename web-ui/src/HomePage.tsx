d454105a04e95d666d37a127cb14c85e703dc8db
Michael Williamson <mike@zwobble.org>
2021-02-18 17:09:07 +0000

    Add link to GDS blog post

--- web-ui/src/HomePage.tsx
+++ web-ui/src/HomePage.tsx
@@ -2,6 +2,9 @@ import {
     Button,
     ButtonGroup,
     Center,
+    Heading,
+    Link,
+    Text,
  } from "@chakra-ui/react";
 
 import { useNavigation } from "./navigation";
@@ -26,6 +29,19 @@ export default function HomePage() {
                     <Button onClick={handleJoinMeetingClick}>Join meeting</Button>
                 </ButtonGroup>
             </Center>
+
+            <Heading as="h2" size="sm" mt={4}>
+                Why use hand signals?
+            </Heading>
+
+            <Text>
+                <Link
+                    color="blue.500"
+                    href="https://gds.blog.gov.uk/2016/10/07/platform-as-a-service-team-takes-even-handed-approach-to-meetings/"
+                >
+                    Inspired by a blog post from the (UK) Government Digital Service (GDS).
+                </Link>
+            </Text>
         </Page>
     );
 }
