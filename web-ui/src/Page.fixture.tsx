e76c87b9984e6a6bf3689cce62dd9a9df498ef6e
Michael Williamson <mike@zwobble.org>
2021-03-04 14:27:00 +0000

    Improve Page fixture

--- web-ui/src/Page.fixture.tsx
+++ web-ui/src/Page.fixture.tsx
@@ -1,7 +1,15 @@
 import Page from "./Page";
 
-export default (
-    <Page title="TITLE">
-        CHILDREN
-    </Page>
-);
+export default {
+    "Default title": (
+        <Page>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </Page>
+    ),
+
+    "Explicit title": (
+        <Page title="Page title">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </Page>
+    ),
+};
