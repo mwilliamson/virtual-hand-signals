45472b5321cb5e0f0931fe7f71e89e82718c2b15
Michael Williamson <mike@zwobble.org>
2021-03-04 15:03:21 +0000

    Improve stories for Page

--- web-ui/src/Page.stories.tsx
+++ web-ui/src/Page.stories.tsx
@@ -7,13 +7,14 @@ export default {
     title: "Page",
     component: Page,
 };
-
-const Template: Story<ComponentProps<typeof Page>> = args => (
-    <Page {...args} />
+export const DefaultTitle = () => (
+    <Page>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </Page>
 );
 
-export const SimplePage = Template.bind({});
-SimplePage.args = {
-    title: "TITLE",
-    children: "CHILDREN",
-};
+export const ExplicitTitle = () => (
+    <Page title="Page title">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </Page>
+);
