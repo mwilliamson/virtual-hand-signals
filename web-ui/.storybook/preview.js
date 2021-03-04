40d007d8287ca1d1c3ec1086fac98e835a5c56fa
Michael Williamson <mike@zwobble.org>
2021-03-04 10:48:59 +0000

    Add Storybook with simple example
    The theming from Chakra UI isn't working correctly due to some
    interaction between Storybook and Chakra UI both using Emotion:
    
        https://github.com/storybookjs/storybook/issues/13114
        https://github.com/chakra-ui/chakra-ui/issues/2527

---
+++ web-ui/.storybook/preview.js
@@ -0,0 +1,14 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+}
+
+export const decorators = [
+  (Story) => (
+    <ChakraProvider>
+      <Story />
+    </ChakraProvider>
+  ),
+];
---
+++ web-ui/src/Page.stories.tsx
@@ -0,0 +1,21 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import React, { ComponentProps } from "react";
+
+import { Story } from "@storybook/react";
+
+import Page from "./Page";
+
+export default {
+    title: "Page",
+    component: Page,
+};
+
+const Template: Story<ComponentProps<typeof Page>> = args => (
+    <Page {...args} />
+);
+
+export const SimplePage = Template.bind({});
+SimplePage.args = {
+    title: "TITLE",
+    children: "CHILDREN",
+};
